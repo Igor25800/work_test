b959368422fb060400a714652e01c35596fb4f5b
Igor Fedor <fedorigor45@gmail.com>
2023-09-12 16:28:57 +0300

    create_form_test_work

--- src/app/shared/services/person.service.ts
+++ src/app/shared/services/person.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {delay, map, Observable, tap} from "rxjs";
+import {delay, map, Observable} from "rxjs";
 import {EmailInterface} from "../interfaces/email.interface";
 import {ValidationErrors} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
